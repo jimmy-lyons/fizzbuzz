c52dbec1dff865ad3131fd12afcd7fc1bbfdb38b
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-04-06 11:34:29 +0100

    refactored

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(number)
-  if number % 3 == 0 && number % 5 != 0
-    "fizz"
-  elsif number % 3 == 0 && number % 5 == 0
+  if number % 3 == 0 && number % 5 == 0
     "fizzbuzz"
-  elsif number % 5 == 0 && number % 3 != 0
+  elsif number % 3 == 0
+    "fizz"
+  elsif number % 5 == 0
     "buzz"
   else
     number
