7acd20b6c74660ec66b230b1690e97333560701d
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-04-06 10:46:10 +0100

    added fizzbuzz and 4

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,10 @@
 def fizzbuzz(number)
   if number == 3
     "fizz"
+  elsif number == 15
+    "fizzbuzz"
+  elsif number == 4
+    4
   else
     "buzz"
   end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,4 +8,12 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
+
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
+  it 'returns "4" when passed 4' do
+    expect(fizzbuzz(4)).to eq 4
+  end
 end
