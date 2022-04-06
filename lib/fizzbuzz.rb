f70eec0a6dfd9b6098f357ee58efd67d6eb5e81a
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-04-06 11:04:11 +0100

    added 30 to tests

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 != 0
     "fizz"
-  elsif number == 15
+  elsif number % 3 == 0 && number % 5 == 0
     "fizzbuzz"
-  elsif number == 5
+  elsif number % 5 == 0 && number % 3 != 0
     "buzz"
   else
     number
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,12 +5,16 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
     
-  it 'returns "fizz" when passed 5' do
+  it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
 
   it 'returns "fizzbuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
+  it 'returns "fizzbuzz" when passed 30' do
+    expect(fizzbuzz(30)).to eq 'fizzbuzz'
   end
 
   it 'returns "4" when passed 4' do
@@ -24,5 +28,9 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 6' do
     expect(fizzbuzz(6)).to eq 'fizz'
   end
-  
+
+  it 'returns "buzz" when passed 10' do
+    expect(fizzbuzz(10)).to eq 'buzz'
+  end
+
 end
